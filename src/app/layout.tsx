7dfe0150b1a22067f93c9ed165c10d9ac7f38707
taeke2 <taeke2@naver.com>
2026-03-24 00:57:57 +0900

    fix: 썸네일 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,33 @@ const notoSansKr = Noto_Sans_KR({
 });
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://mobile-invitation-sigma.vercel.app/"),
+
     title: "성택🖤현정 결혼합니다",
     description: "5월 9일 (토요일) 오후 6시 30분",
+
+    openGraph: {
+        title: "성택🖤현정 결혼합니다",
+        description: "5월 9일 (토요일) 오후 6시 30분",
+        siteName: "성택🖤현정 모바일 청첩장",
+        images: [
+            {
+                url: "/images/main/main_image_02.jpg",
+                width: 1200,
+                height: 630,
+                alt: "성택 현정 결혼식",
+            },
+        ],
+        locale: "ko_KR",
+        type: "website",
+    },
+
+    twitter: {
+        card: "summary_large_image",
+        title: "성택🖤현정 결혼합니다",
+        description: "5월 9일 (토요일) 오후 6시 30분",
+        images: ["/images/main/main_image_02.jpg"],
+    },
 };
 
 export default function RootLayout({
