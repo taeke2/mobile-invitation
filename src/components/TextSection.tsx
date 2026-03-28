dbdf0149c0d3c2a32e8ec5b1aa0d6773f7dec11b
taeke2 <taeke2@naver.com>
2026-03-29 06:03:02 +0900

    style: 텍스트 섹션 글자 수정

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -4,7 +4,7 @@ import FadeInOnView from "@/src/components/FadeInOnView";
 
 export default function TextSection() {
     return (
-        <section className="px-6 py-24 text-center text-black">
+        <section className="flex px-6 py-24 text-center text-black">
             <div className="mx-auto">
                 {/* 타이틀 */}
                 <FadeInOnView>
@@ -14,7 +14,7 @@ export default function TextSection() {
                 </FadeInOnView>
 
                 {/* 본문(시/문구) */}
-                <div className="mt-10 text-[12px] font-gowun-batang leading-relaxed">
+                <div className="mt-10 text-[12px] font-nanum-myeongjo leading-relaxed">
                     <FadeInOnView>
                         <p>
                             사랑은 소유가 아니라<br/>
@@ -29,15 +29,13 @@ export default function TextSection() {
                             서로의 두 눈을 고요히 바라보아<br/>
                             말하지 않아도 같은 쪽으로 걸어가리라<br/><br/>
 
-                            -박미라, ‘아름다운 날에 부치다’ 중에서<br/><br/>
+                            - 박미라, ‘아름다운 날에 부치다’ 중에서 -<br/><br/>
                         </p>
                     </FadeInOnView>
                     <FadeInOnView>
-                        <p>
-                            저희 두 사람 이제 믿음과 사랑으로<br/>
-                            한 길을 가려 합니다.<br/><br/>
-
-                            그 시작의 한 걸음, 함께 축복해 주시면 감사하겠습니다.
+                        <p className="mt-3 font-bold">
+                            서로의 곁을 선택한 두 사람이 이제 함께합니다.<br/>
+                            그 시작을 함께해 주시면 감사하겠습니다.
                         </p>
                     </FadeInOnView>
                 </div>
@@ -45,8 +43,16 @@ export default function TextSection() {
 
                 {/* 하단 이름 */}
                 <FadeInOnView>
-                    <div className="font-nanum-myeongjo font-bold mt-10 text-[12px]">
-                        신랑 허성택 · 신부 이현정
+                    <div className="mt-10 text-center font-nanum-myeongjo text-[12px] font-bold">
+                        <div className="inline-grid grid-cols-[auto_auto_auto] gap-x-3 gap-y-0.5">
+                            <span className="text-right">허정행 · 박성연의</span>
+                            <span>아들</span>
+                            <span>허성택</span>
+
+                            <span className="text-right">이형석 · 류정란의</span>
+                            <span>딸</span>
+                            <span>이현정</span>
+                        </div>
                     </div>
                 </FadeInOnView>
             </div>
